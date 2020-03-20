18b81ed789eea482651fedae06a068639eaca345
Kuldeep Jagrotiya <kuldeep@ALIPL0858.Dlink>
2020-03-20 17:57:12 +0530

    removed functions assignment solution

--- function/OddEven.py
+++
@@ -1,8 +0,0 @@
-## Q2 print odd and even number from given range
-
-num = int(input("enter the number : "))
-for i in range(num+1):
-    if i%2==0:
-        print (i,"is even")
-    elif i%2!=0:
-        print(i,"is odd")
